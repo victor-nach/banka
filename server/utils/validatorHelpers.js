1e8074d3d043b57e9c105012841ec1b4cb352994
Kunrad <your.email@example.com>
2019-04-25 16:57:23 +0100

    feature(swagger): integrate swagger UI
    - remove unrelated file changes
    
    [Finishes #165496757]

--- server/utils/validatorHelpers.js
+++ server/utils/validatorHelpers.js
@@ -112,7 +112,7 @@ class ValidatorHelpers {
    */
   static checkNumber(route, ...input) {
     input.forEach((element) => {
-      route.push(check(element).trim().isNumeric().withMessage(`please put in a valid number as ${element} with no signs`));
+      route.push(check(element).trim().isNumeric().withMessage(`please put in a valid number as ${element}`));
     });
   }
 }
